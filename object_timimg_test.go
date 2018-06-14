54eb0347f5491bb27170e915d84d2baef7025612
Quentin DEQUELEN <qdequele@e2r6p8.42.fr>
2018-06-14 18:58:51 +0200

    add test + bench

---
+++ object_timimg_test.go
@@ -0,0 +1,65 @@
+package jsonQuerry
+
+import (
+	"testing"
+)
+
+func BenchmarkKeepSmallSimple(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepSmallSimple()
+	}
+}
+
+func BenchmarkKeepSmallMedium(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepSmallMedium()
+	}
+}
+func BenchmarkKeepSmallHard(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepSmallHard()
+	}
+}
+
+func BenchmarkKeepMediumSimple(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepMediumSimple()
+	}
+}
+
+func BenchmarkKeepMediumMedium(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepMediumMedium()
+	}
+}
+func BenchmarkKeepMediumHard(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepMediumHard()
+	}
+}
+
+func BenchmarkKeepLargeSimple(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepLargeSimple()
+	}
+}
+
+func BenchmarkKeepLargeMedium(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepLargeMedium()
+	}
+}
+func BenchmarkKeepLargeHard(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		KeepLargeHard()
+	}
+}
